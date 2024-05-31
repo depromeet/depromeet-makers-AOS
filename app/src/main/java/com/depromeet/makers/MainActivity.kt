b97cdef63a1c52ca98f76b88d9b73be561ccf34a
ChuYong <jakgon@naver.com>
2024-05-31 11:51:06 +0900

    feat: 앱 테마, 아이콘 등 적용

--- app/src/main/java/com/depromeet/makers/MainActivity.kt
+++ app/src/main/java/com/depromeet/makers/MainActivity.kt
@@ -7,12 +7,19 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        var isKeepSplash = true
         setContentView(R.layout.activity_main)
+
+        splashScreen.setKeepOnScreenCondition {
+            isKeepSplash
+        }
 
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -21,7 +28,12 @@ class MainActivity : AppCompatActivity() {
 
         val webView = findViewById<WebView>(R.id.webview)
 
-        val webViewClient = WebViewClient()
+        val webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                isKeepSplash = false
+            }
+        }
         webView.webViewClient = webViewClient
         webView.webChromeClient = WebChromeClient()
 
